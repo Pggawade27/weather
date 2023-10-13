7a0a05100efd46f4559ce26dfe82a657658143fd
prasad <prasadgawade249@gmail.com>
2023-10-13 15:43:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("search-button");
+    const searchInput = document.getElementById("search");
+    const location = document.getElementById("location");
+    const temperature = document.getElementById("temperature");
+    const description = document.getElementById("description");
+    const wind = document.getElementById("wind");
+
+    searchButton.addEventListener("click", function () {
+        const city = searchInput.value;
+        if (city) {
+            getWeather(city);
+        }
+    });
+
+    function getWeather(city) {
+        // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+        const apiKey = "0db83138bfffc5599d46f1fcff33a2a5";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                location.textContent = "Weather in " + data.name;
+                temperature.textContent = `${(data.main.temp - 273.15).toFixed(2)}°C`;
+                description.textContent = data.weather[0].description;
+                wind.textContent = data.wind.speed + "  km/h";
+            })
+            .catch((error) => {
+                console.log("Error: ", error);
+            });
+    }
+});
